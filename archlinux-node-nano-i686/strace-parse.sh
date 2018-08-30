61a4feccccef05cc03a56d3ec74bc5199493c338
Andrii Melnykov <andy.melnikov@gmail.com>
2018-08-30 22:35:53 +0300

    Inline strace-parse.sh
    The code is still modular, but function strace_parse is now
    physically in strace-spec.sh
    
    The intermediate app.parsed file is stil created though
    for debugging purposes

--- archlinux-node-nano-i686/strace-parse.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -ex -o pipefail
-cat $1 \
-	| sed -e 's|^[0-9]*  *||;' \
-       	| grep -Ev '^[+\-]|= -1 E[A-Z]+ \([A-Z][A-Za-z ]+\)$' \
-       	| sed -e 's|^open("\(.*\)", O_.* = [0-9]*$|\1|;s|^execve("\(.*\)", \[.*], 0x.* .*) = [0-9]*|\1|;s|^openat(AT_FDCWD, "\(.*\)", O_.*|\1|' | sort -u
--- archlinux-node-nano-i686/strace-spec.sh
+++ archlinux-node-nano-i686/strace-spec.sh
@@ -4,8 +4,16 @@ set -e -o pipefail
 DIR=$(dirname $0)
 IN=$1
 ROOT=$2
+TRACEFILE=app.trace
 
 [ -d $2 ] || (echo "Argument $2 is not a dir" ; exit -1)
+
+function strace_parse {
+	cat \
+		| sed -e 's|^[0-9]*  *||;' \
+		| grep -Ev '^[+\-]|= -1 E[A-Z]+ \([A-Z][A-Za-z ]+\)$' \
+		| sed -e 's|^open("\(.*\)", O_.* = [0-9]*$|\1|;s|^execve("\(.*\)", \[.*], 0x.* .*) = [0-9]*|\1|;s|^openat(AT_FDCWD, "\(.*\)", O_.*|\1|' | sort -u
+}
 
 function grep_nosys {
 	grep -vE '^/(dev|sys|run|tmp|proc)/|^(/etc/ld.so.cache|/|/var/cache/ldconfig/aux-cache)$';
@@ -14,7 +22,7 @@ function grep_nosys {
 (
 node $DIR/dir-links.js <(
 	(
-	cat $IN
+	cat $TRACEFILE | strace_parse | tee $IN
 	find -L /lib -maxdepth 1 -name 'libnss_files.so*' -or -name 'libnss_dns.so*' -or -name 'ld-linux.so*' -or -name 'libresolv.so*'
 	find -L /etc -maxdepth 1 -name 'hosts'
 	) | grep_nosys
