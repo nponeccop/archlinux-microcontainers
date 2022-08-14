be07b2775e01769a63c82337df1b8f3f992d8817
Andrii Melnykov <andy.melnikov@gmail.com>
2022-08-14 00:48:02 +0000

    Auto-copy smith-strace on build

--- rockylinux8-gearmand/build.sh
+++ rockylinux8-gearmand/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -ex -o pipefail
+rm -r smith-strace || true
+cp -r ../archlinux-node-nano-i686/smith-strace/ .
 podman build . -t small-tar -f Dockerfile.node
 podman rm small-tar || true
 podman create --name small-tar small-tar /bin/dummycmd
