8f41521d5ca626a5c161db6e69aab0de29fcc943
Andrii Melnykov <andy.melnikov@gmail.com>
2022-08-14 00:46:43 +0000

    Refactor into default-trace, strace-spec and default-mkrootfs

--- archlinux-node-nano-i686/smith-strace/default-mkrootfs.sh
+++ archlinux-node-nano-i686/smith-strace/default-mkrootfs.sh
@@ -2,3 +2,4 @@
 
 mkdir /rootfs
 rsync -alv / /rootfs --files-from=app.spec
+tar --numeric-owner -C /rootfs -cvf /rootfs.tar .
--- archlinux-node-nano-i686/smith-strace/default-trace.sh
+++ archlinux-node-nano-i686/smith-strace/default-trace.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-DIR=$(dirname $0)
-
 timeout 5 strace -o app.trace -s 65536 -e trace=openat,execve -e signal=none -fyqq $* || true
-$DIR/strace-spec.sh app.trace /
